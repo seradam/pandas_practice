c455103a611edfe65a544f51cff63d507a1a93e3
seradam <seradam89@gmail.com>
2017-06-28 11:56:04 +0200

    crosstab with percentages

--- jazz.py
+++ jazz.py
@@ -20,31 +20,40 @@ helper_dict = {}
 for i in range(len(column_names)):
     helper_dict[column_names[i]] = new_column_names[i]
 data.rename(columns=helper_dict)
-# print(list(data.columns.values))
 
 # drop off the unnecessary columns:
 
 new_data = data[[b'ID', b'szulev', b'nem', b'szul_telepules', b'telepules', b'vegzettseg', b'zeneiskola', b'reteg', b'reteg10', b'apa_vegzettseg', b'anya_vegzettseg', b'zenesz_csaladban', b'penz', b'penz_tamogatas', b'megelhetesi_keszseg', b'zeneiskh_felkesz', b'zeneiskh_kapcsolat', b'zeneiskh_anyagi', b'fellepesbol_megelni', b'megelesi_hanyad']]
 
-# print(new_data)
-
 # frequency and basic statistics:
 
+print('\nfrequencies of birth year:\n')
 print(new_data[b'szulev'].value_counts())
 
 new_data_a = new_data.copy() # I use copy because of SettingWithCopyWarning
 
 new_data_a.loc[:, b'eletkor'] = 2017 - new_data[b'szulev']
 
-print(new_data_a[b'eletkor'])
-
+print('\nmean of the age:\n')
 print(new_data_a[b'eletkor'].mean())
 
+print('\nmedian of the age:\n')
 print(new_data_a[b'eletkor'].mode())
 
+print('\nstandard deviation of the age:\n')
 print(new_data_a[b'eletkor'].std())
 
+print('\ncrosstab gender x education with frequencies:\n')
 print(pd.crosstab(new_data_a[b'vegzettseg'], new_data_a[b'nem'], rownames=['vegzettseg'], colnames=['nem']))
+
+# There is no built-in function for display percentage, but it is possible with lambda
+# column percentage: axis=0 ; row percentage: axis=1
+
+print('\ncrosstab gender x education with percentages:\n')
+print(pd.crosstab(new_data_a[b'vegzettseg'], new_data_a[b'nem']).apply(lambda r: r/r.sum(), axis=0))
+
+# Display frequency and percentage in one table is possible, but very complex and ugly, I don't suggest it
+
 
 # new_data_a[b'nem'].plot.pie(figsize=(6, 6))
 # plt.pie(new_data_a[b'nem'])
