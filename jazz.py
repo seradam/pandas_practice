770c259de93fa14e14969b1b151b3f855219383a
seradam <seradam89@gmail.com>
2017-06-29 16:03:38 +0200

    chart repaired

--- jazz.py
+++ jazz.py
@@ -55,8 +55,7 @@ print(pd.crosstab(new_data_a[b'vegzettseg'], new_data_a[b'nem']).apply(lambda r:
 # Display frequency and percentage in one table is possible, but very complex and ugly, I don't suggest it
 
 
-# new_data_a[b'nem'].plot.pie(figsize=(6, 6))
-# plt.pie(new_data_a[b'nem'])
-# new_data_a.plot(x=b'nem', y=b'vegzettseg', kind='scatter', xlim=[0, 0.0006])
-new_data_a.plot(x=b'nem', y=b'vegzettseg', kind='pie')
+# gender distribution iin a pie chart
+
+new_data_a[b'nem'].value_counts().plot.pie(labels=['male', 'female'])
 plt.show()
